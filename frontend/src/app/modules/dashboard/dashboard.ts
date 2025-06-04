6bd001d75cdbece1bdfa0f70445895b067da55ca
vishnupriyaab <vishnupriyaotp2002@gmail.com>
2025-06-05 03:10:44 +0530

    dashboard side menu is done

--- frontend/src/app/modules/dashboard/dashboard.ts
+++ frontend/src/app/modules/dashboard/dashboard.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { Menu } from "../menu/menu";
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  standalone:true,
+  imports: [Menu],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css'
 })
---
+++ frontend/src/app/modules/menu/menu.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu',
+  standalone:true,
+  imports: [],
+  templateUrl: './menu.html',
+  styleUrl: './menu.css'
+})
+export class Menu {
+
+}
