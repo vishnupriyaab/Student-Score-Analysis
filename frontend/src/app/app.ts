32f47389df7270c748913aded7a023fcb41740e8
vishnupriyaab <vishnupriyaotp2002@gmail.com>
2025-06-04 22:54:36 +0530

    login page UI is done

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  standalone: true,
+  imports: [RouterModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
